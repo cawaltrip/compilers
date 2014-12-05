3646b5897bc5cd4a9aaedfa430564cadb2b4ed77
Chris Waltrip <walt2178@vandals.uidaho.edu>
2014-12-05 11:26:57 -0800

    Modified stub code to test function definition symbol table entries

--- stubs/5.cpp
+++ stubs/5.cpp
@@ -1,50 +1,19 @@
-//int times_two(int x);
-int times_two(int x) {
-	int ret = x * 2;
-	return ret;
+int times_two(int x);
+
+int main(int argc, char argv) {
+  //char *my_string = "Hello world!";
+  long long_var;
+  short short_var = 2;
+  cout << my_string << endl;
+
+  return 0;
 }
 
-/*
-Results:
+float pi() {
+  return 3.14159;
+}
 
-decl-seq-2 (149002,2)
-  simple-decl-1 (152001,3)
-    322: int
-    init-decl-1 (174001,2)
-      direct-decl-5 (176005,4)
-        258: times_two
-        40: (
-        param-decl-1 (187001,2)
-          322: int
-          258: x
-        41: )
-    59: ;
-  func-decl-2 (188002,4)
-    322: int
-    direct-decl-5 (176005,4)
-      258: times_two
-      40: (
-      param-decl-1 (187001,2)
-        322: int
-        258: f
-      41: )
-    cmpd-stmt-1 (141001,3)
-      123: {
-      stmt-seq-2 (142002,2)
-        simple-decl-1 (152001,3)
-          322: int
-          init-decl-1 (174001,2)
-            258: ret
-            init-1 (190001,2)
-              61: =
-              mult-expr-2 (123002,3)
-                258: f
-                42: *
-                259: 2
-          59: ;
-        jump-stmt-3 (147003,3)
-          333: return
-          258: ret
-          59: ;
-      125: }
-*/
+int times_two(int z) {
+	int ret = z * 2;
+	return ret;
+}
